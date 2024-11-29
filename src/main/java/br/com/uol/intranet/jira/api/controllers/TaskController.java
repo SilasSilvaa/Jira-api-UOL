12bac4e2ea5df815a9c693a13e83a57365e151b8
silassilvaa <silas-oli@hotmail.com>
2024-11-29 17:30:57 -0300

    feat: add task controller

---
+++ src/main/java/br/com/uol/intranet/jira/api/controllers/TaskController.java
@@ -0,0 +1,25 @@
+package br.com.uol.intranet.jira.api.controllers;
+
+import br.com.uol.intranet.jira.api.domain.task.TaskResponse;
+import br.com.uol.intranet.jira.api.services.JiraTaskService;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jira/issue")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final JiraTaskService jiraService;
+
+    @GetMapping("/{task}")
+    public ResponseEntity<TaskResponse> getTask(@PathVariable @NotBlank String task){
+        TaskResponse data = jiraService.getTask(task);
+        return ResponseEntity.ok(data);
+    }
+}
