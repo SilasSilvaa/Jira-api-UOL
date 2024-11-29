c38a990b27f553767c3425a8e2faa55f1f537ad0
silassilvaa <silas-oli@hotmail.com>
2024-11-29 19:24:43 -0300

    feat: add request Get (allTasks)

--- src/main/java/br/com/uol/intranet/jira/api/controllers/TaskController.java
+++ src/main/java/br/com/uol/intranet/jira/api/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.uol.intranet.jira.api.controllers;
 
 import br.com.uol.intranet.jira.api.domain.task.TaskResponse;
+import br.com.uol.intranet.jira.api.domain.task.Issue;
 import br.com.uol.intranet.jira.api.services.JiraTaskService;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,10 @@ public class TaskController {
     public ResponseEntity<TaskResponse> getTask(@PathVariable @NotBlank String task){
         TaskResponse data = jiraService.getTask(task);
         return ResponseEntity.ok(data);
+    }
+
+    @GetMapping
+    public ResponseEntity<Issue> getTasks(){
+        return ResponseEntity.ok(jiraService.getTasks());
     }
 }
