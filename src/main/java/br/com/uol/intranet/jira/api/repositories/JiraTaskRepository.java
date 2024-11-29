54c3fb61982a2b5c6863cc050718b4af4fdae2eb
silassilvaa <silas-oli@hotmail.com>
2024-11-29 19:23:39 -0300

    feat: add method get Tasks

--- src/main/java/br/com/uol/intranet/jira/api/repositories/JiraTaskRepository.java
+++ src/main/java/br/com/uol/intranet/jira/api/repositories/JiraTaskRepository.java
@@ -1,8 +1,9 @@
 package br.com.uol.intranet.jira.api.repositories;
 
 import br.com.uol.intranet.jira.api.domain.task.TaskResponse;
-
+import br.com.uol.intranet.jira.api.domain.task.Issue;
 
 public interface JiraTaskRepository {
+    Issue getTasks();
     TaskResponse getTask(String task);
 }
