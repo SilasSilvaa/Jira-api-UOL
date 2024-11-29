34a83a5c626d3a67001699a66fe75172a366720b
silassilvaa <silas-oli@hotmail.com>
2024-11-29 19:24:16 -0300

    feat: add getTasks method implementation

--- src/main/java/br/com/uol/intranet/jira/api/services/JiraTaskService.java
+++ src/main/java/br/com/uol/intranet/jira/api/services/JiraTaskService.java
@@ -1,6 +1,7 @@
 package br.com.uol.intranet.jira.api.services;
 
 import br.com.uol.intranet.jira.api.domain.task.TaskResponse;
+import br.com.uol.intranet.jira.api.domain.task.Issue;
 import br.com.uol.intranet.jira.api.repositories.JiraTaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class JiraTaskService implements JiraTaskRepository {
 
     private final WebClient webClient;
+
+    @Override
+    public Issue getTasks() {
+        try{
+            return webClient
+                    .get()
+                    .uri("/search?jql=project=SC&maxResults=10&startAt=0")
+                    .retrieve()
+                    .bodyToMono(Issue.class)
+                    .block();
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
 
     @Override
     public TaskResponse getTask(String task) {
